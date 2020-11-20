034c1dac35b95a3777d708aa1aa5b9f5afb349b2
majahoberg <eaamhob@students.eaaa.dk>
2020-11-20 16:36:06 +0100

    ;

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
-import { firestorePlugin } from "vuefire"
+import { firestorePlugin } from "vuefire";
 
 Vue.config.productionTip = false;
 Vue.use(firestorePlugin);
